276eb0e050a6a1c6abe12308842823e7fafd2080
LeslieAyacocho <ayacocho.leslie@gmail.com>
2025-04-07 15:38:48 +0800

    feaet: add web development projects

--- src/components/works/web-developments.js
+++ src/components/works/web-developments.js
@@ -1,5 +1,6 @@
 import Container from '@/components/ui/container'
 import Card from '@/components/ui/card'
+import Link from 'next/link'
 
 const WebDevelopments = () => {
     return (
@@ -8,11 +9,16 @@ const WebDevelopments = () => {
                 <div className='px-8 py-3 border bg-li-blue w-max border-li-black'>
                     <h2 className='text-2xl font-medium uppercase text-li-white'>Web Development</h2>
                 </div>
-                <div className='grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-4 md:gap-10'>
-                    {/* <Card image={'/web-design/apracsi.png'} title='APRACSI' description='2024' />
-                    <Card image={'/web-design/gk-usa.png'} title='GK - USA' description='2023' />
-                    <Card image={'/web-design/csapi.png'} title='CSAPI' description='2023' />
-                    <Card image={'/web-design/cliqq.png'} title='CLiQQ - Redesign' description='2022' /> */}
+                <div className='grid grid-cols-1 gap-14 md:grid-cols-2 lg:grid-cols-3 md:gap-10'>
+                    <a href='https://impact.outsourceaccelerator.com/' target='_blank'>
+                        <Card image={'/web-development/oir-2024.png'} title='OIR' description='2024' />
+                    </a>
+                    <Link href='/project/oats'>
+                        <Card image={'/web-development/oats-2022.png'} title='OATS' description='2022' />
+                    </Link>
+                    <Link href='/project/cryptonews'>
+                        <Card image={'/web-development/cryptonews-2021.png'} title='CryptoNews' description='2021' />
+                    </Link>
                 </div>
             </Container>
         </section>
