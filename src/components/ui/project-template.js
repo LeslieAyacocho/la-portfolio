9f18b08d8504d94619e02ce1388acddcb279d584
LeslieAyacocho <ayacocho.leslie@gmail.com>
2025-04-07 15:16:02 +0800

    feat: update project template component

--- src/components/ui/project-template.js
+++ src/components/ui/project-template.js
@@ -2,51 +2,46 @@ import Image from 'next/image'
 
 const ProjectTemplate = ({ data }) => {
     return (
-        <div className='flex flex-col px-2 gap-y-4 text-li-black'>
-            <div className='px-4 py-3 border bg-li-blue w-max border-li-black'>
-                <h2 className='text-4xl font-semibold uppercase text-li-white'>{data?.title}</h2>
+        <div className='flex flex-col md:px-2 gap-y-4 text-li-black'>
+            <div className='px-2'>
+                <h2 className='text-4xl font-bold text-li-black'>{data?.title}</h2>
             </div>
-            <div className='max-w-screen-lg px-2 '>
+            <div className='px-2'>
                 <h3 className='text-xl font-semibold uppercase text-li-black'>About</h3>
                 <p className='py-3 text-lg'>{data?.about}</p>
             </div>
-            <div className='flex w-full max-w-screen-lg gap-x-8'>
+
+            <div className='flex flex-col gap-y-4'>
                 {data?.role && (
-                    <div className='w-full uppercase border border-li-black h-min'>
-                        <h3 className='p-2 font-semibold border-b border-li-black bg-li-blue text-li-white'>
-                            {data?.role?.length > 1 ? 'Roles' : 'Role'}
-                        </h3>
-                        <div className='flex flex-col p-2 bg-white'>
-                            {data?.role?.map((item, index) => (
-                                <p key={`roles-${index}`} className='text-li-black'>
-                                    {item}
-                                </p>
+                    <div className='px-2 w-full'>
+                        <h3 className='text-xl font-semibold uppercase text-li-black'>Roles</h3>
+                        <ul className='p-2 flex flex-col gap-y-1'>
+                            {data?.role.map((item) => (
+                                <li>{item}</li>
                             ))}
-                        </div>
+                        </ul>
                     </div>
                 )}
                 {data?.tech && (
-                    <div className='w-full border border-li-black h-min'>
-                        <h3 className='p-2 font-semibold uppercase border-b border-li-black bg-li-blue text-li-white'>Technologies Used</h3>
-                        <div className='p-2 bg-white text-li-black'>
-                            <a href={data?.url} target='_blank'>
-                                {data?.url}
-                            </a>
-                        </div>
+                    <div className='px-2 w-full'>
+                        <h3 className='text-xl font-semibold uppercase text-li-black'>Technologies Used</h3>
+                        <ul className='p-2 flex flex-col gap-y-1'>
+                            {data?.tech.map((item) => (
+                                <li>{item}</li>
+                            ))}
+                        </ul>
                     </div>
                 )}
                 {data?.url && (
-                    <div className='w-full border border-li-black h-min'>
-                        <h3 className='p-2 font-semibold uppercase border-b border-li-black bg-li-blue text-li-white'>Links</h3>
-                        <div className='p-2 bg-white text-li-black'>
-                            {data?.url?.map((item, index) => (
-                                <div key={index} className='flex items-center uppercase'>
-                                    <a href={item.link} target='_blank' rel='noopener noreferrer'>
-                                        {item.text}
-                                    </a>
-                                </div>
+                    <div className='px-2 w-full'>
+                        <h3 className='text-xl font-semibold uppercase text-li-black'>Links</h3>
+                        <ul className='p-2 flex flex-col gap-y-1'>
+                            {data?.url.map((item) => (
+                                <li className='underline'>
+                                    <a href={item?.link}>{item?.text}</a>
+                                </li>
                             ))}
-                        </div>
+                        </ul>
                     </div>
                 )}
             </div>
