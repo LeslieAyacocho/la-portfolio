c25a862b95196ff73e9735bc790d0c0200089b0b
LeslieAyacocho <ayacocho.leslie@gmail.com>
2025-04-09 15:48:03 +0800

    feat: update path

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -4,7 +4,7 @@ const Card = ({ title, description, image }) => {
     return (
         <div className='w-full bg-white border divide-y cursor-pointer border-li-black divide-li-black'>
             <div className='overflow-hidden'>
-                <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH}/img${image}`} width={600} height={600} className='object-cover w-full h-full' />
+                <Image src={`/la-portfolio/img${image}`} width={600} height={600} className='object-cover w-full h-full' />
             </div>
             <div className='p-5'>
                 <h3 className='text-lg font-semibold tracking-tight uppercase md:text-2xl text-li-black'>{title}</h3>
